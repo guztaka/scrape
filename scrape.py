32a7028cb68bb22f0725275cdf1232018f0f4984
guzman-takayuki <guzman-takayuki@F-Ebisu-0072.local>
2018-03-27 17:35:30 +0900

    modify

--- scrape.py
+++ scrape.py
@@ -3,11 +3,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://shikaku-fan.net/英検（実用英語技能検定）/'
+url = 'http://shikaku-fan.net/national_qualification_rank.php'
 
 res = requests.get(url)
 data = BeautifulSoup(res.text, 'html.parser')
-print('資格のタイトル：　' + data.select_one('#big').text)
-print('資格のカテゴリ：　' + data.select_one('#main_h > div.type2').text)
-print('資格の難易度：　' + data.find_all('table', {'class': 'table555'}).text)
-# print('資格の難易度：　' + data.select_one('#main_h > table.table555 > tbody > tr:nth-child(1) > th:nth-child(1)').text)
+# title = data.select_one('#big').text
+# category = data.select_one('#main_h > div.type2').text
+# print('資格のタイトル：　' + data.select_one('#big').text)
+# print('資格のカテゴリ：　' + data.select_one('#main_h > div.type2').text)
+# print('資格の難易度：　' + data.find_all('table', {'class': 'table555'}).text)
+# # print('資格の難易度：　' + data.select_one('#main_h > table.table555 > tbody > tr:nth-child(1) > th:nth-child(1)').text)
+
+
+tables = data.findAll('table', class_='table222')
+
+try:
+    for queue in tables:
+        for tr in queue.findAll('tr'):
+            print(tr.a)
+            try:
+                print(tr.a['href'])

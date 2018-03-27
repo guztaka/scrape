2de610107346ba35c9f2c7d15eabd82d1a98f17b
Takayuki Guzman <takayuki@quuz.jp>
2018-03-27 18:36:13 +0900

    script finish

--- scrape.py
+++ scrape.py
@@ -2,24 +2,29 @@
 
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-url = 'http://shikaku-fan.net/national_qualification_rank.php'
+url = 'URL/TO/HERE'
 
 res = requests.get(url)
 data = BeautifulSoup(res.text, 'html.parser')
-# title = data.select_one('#big').text
-# category = data.select_one('#main_h > div.type2').text
-# print('資格のタイトル：　' + data.select_one('#big').text)
-# print('資格のカテゴリ：　' + data.select_one('#main_h > div.type2').text)
-# print('資格の難易度：　' + data.find_all('table', {'class': 'table555'}).text)
-# # print('資格の難易度：　' + data.select_one('#main_h > table.table555 > tbody > tr:nth-child(1) > th:nth-child(1)').text)
 
+tables = data.findAll('table', class_='table222')  # why table222?
 
-tables = data.findAll('table', class_='table222')
+# csv file open
+with open('kokka.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
 
-try:
+    # reading tables and writing to file
     for queue in tables:
         for tr in queue.findAll('tr'):
-            print(tr.a)
             try:
-                print(tr.a['href'])
+                # print(f"リンク：{tr.a['href']}")
+                # print(f"資格名：{tr.a.text}")
+                # print(f"偏差値：{tr.span.text}")
+                data = [tr.a['href'], tr.a.text, tr.span.text]
+                writer.writerow(data)
+            except:
+                pass
+
+    f.close()
